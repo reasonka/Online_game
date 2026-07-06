5917e97e44303747eea552a0ddc7ee0cabbb5d4b
lunatic-d <chanyikka03@gmail.com>
2026-07-07 02:37:13 +0800

    chicken banana

--- Online_game/Assets/Scripts/InfoPanelUI.cs
+++ Online_game/Assets/Scripts/InfoPanelUI.cs
@@ -1,44 +1,14 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class InfoPanelUI : MonoBehaviour
 {
     public GameObject infoPanel;
-
-    [Header("Text Columns")]
-    public TMP_Text keyText;
-    public TMP_Text actionText;
 
     public KeyCode toggleKey = KeyCode.Tab;
     public bool showOnStart = false;
 
     private void Start()
     {
-        if (keyText != null)
-        {
-            keyText.text =
-                "WASD\n" +
-                "E\n" +
-                "Q\n" +
-                "R\n" +
-                "Arrow Keys\n" +
-                "Enter\n" +
-                "Left Ctrl";
-        }
-
-        if (actionText != null)
-        {
-            actionText.text =
-                "Walk\n" +
-                "Pick up food\n" +
-                "Drop food\n" +
-                "Open emoji menu\n" +
-                "Choose emoji\n" +
-                "Confirm emoji\n" +
-                "Unlock cursor";
-        }
-
         SetPanelVisible(showOnStart);
     }
 
